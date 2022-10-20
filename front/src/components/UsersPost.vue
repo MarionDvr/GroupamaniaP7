60fa4a0e4cb0902ac413e2df4e19a9889b1e005f
Marion Devroede <marion.devroede@hotmail.fr>
2022-10-20 09:22:40 +0200

    Correction erreur UsersPost

---
+++ front/src/components/UsersPost.vue
@@ -0,0 +1,29 @@
+<script>
+    export default {
+        name: "UsersPost"
+        }
+</script>
+<template>
+    <article>
+        <div>
+            <figure>
+                <img/>
+            </figure>
+            <p></p>
+        </div>
+        <div>
+            <h2></h2>
+            <figure>    
+                <img/>
+            </figure>
+            <p></p>
+        </div>
+        <div>
+            <i class="fa-solid fa-heart"></i>
+            <i class="fa-sharp fa-solid fa-pen"></i>
+            <i class="fa-solid fa-trash"></i>
+        </div>
+    </article>
+</template>
+<style lang="scss">
+</style>
