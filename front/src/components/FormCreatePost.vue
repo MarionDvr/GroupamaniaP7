aad2a58e26eb5089c48933e52194feb5f89487ac
Marion Devroede <marion.devroede@hotmail.fr>
2022-10-20 09:18:54 +0200

    Ajout des pages

---
+++ front/src/components/FormCreatePost.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        name: "FormCreatePost"
+    }
+</script>
+<template>
+    <section>   
+        <h2></h2>
+        <form>
+            <label>Titre</label>
+            <input/>
+            <label>Image</label>
+            <input/>
+            <label>Texte</label>
+            <input/>
+            <button>Poster !</button>
+        </form>
+    </section>
+</template>
+<style lang="scss">
+</style>
