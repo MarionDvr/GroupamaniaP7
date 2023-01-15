380daabda353dcbe3e3d8aa18df5741f5bf53865
Marion Devroede <marion.devroede@hotmail.fr>
2023-01-15 16:01:46 +0100

    Ajout de helmet

--- back/app.js
+++ back/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const postRoutes = require('./routes/post');
 const userRoutes = require('./routes/user');
 const app = express();
@@ -26,8 +27,10 @@ app.use((req, res, next) => {
     next();
   });
 
-//Attribution des middlewares aux bonnes routes
+
 app.use(express.json());
+app.use(helmet());
+//Attribution des middlewares aux bonnes routes
 app.use('/api/posts', postRoutes);
 app.use('/api/auth', userRoutes);
 
